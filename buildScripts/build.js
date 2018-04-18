1a5cbc70d749a554cd6de1120ebda5c8598a6b87
Manu Prathapan <manuprathapan92@gmail.com>
2018-04-18 14:24:12 +0530

    Added code to build for production

---
+++ buildScripts/build.js
@@ -0,0 +1,34 @@
+
+/* eslint-disable no-console */
+import webpack from 'webpack';
+import chalk from 'chalk';
+import webpackConfig from '../webpack.config.prod';
+
+process.env.NODE_ENV = 'production';
+
+console.log(chalk.blue('Generating minified bundle for production. This will take a moment...'));
+
+webpack(webpackConfig).run((err, stats) => {
+  if (err) {
+    console.log(chalk.red(err));
+    return 1;
+  }
+
+  //code to display error or warings
+  const jsonStatus = stats.toJson();
+
+  if (jsonStatus.hasErrors) {
+    return jsonStatus.errors.map(error => console.log(chalk.red(error)));
+  }
+
+  if (jsonStatus.hasWarnings) {
+    console.log(chalk.yellow('Webpack generated the following warnings: '));
+    jsonStatus.warnings.map(warning => console.log(chalk.yellow(warning)));
+  }
+
+  console.log(`Webpack stats: ${stats}`);
+
+  console.log(chalk.green("Your app has been built for production and written to /dist"));
+
+  return 0;
+});
