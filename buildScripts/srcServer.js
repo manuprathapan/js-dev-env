5b013e6edcbd3fd0be3f9cae26f829ba9fd7000c
Manu Prathapan <manuprathapan92@gmail.com>
2018-04-18 11:02:50 +0530

    Added changes for linitng and testing

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,19 @@
 import express from 'express';
 import path from 'path'; //reference to path
 import open from 'open'; //reference to open our site in browser
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+
+/* eslint-disable no-console */
 
 const port = 3000; //port that will be used
 const app = express(); //creates an instance of express
+const compiler = webpack(config); //Reference to webpack compiler
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'))
